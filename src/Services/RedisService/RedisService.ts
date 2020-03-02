0b275f9a0f5acfd8e730f5e50c1eafdfb6522790
0x2CA <2478557459@qq.com>
2020-03-02 15:24:56 +0800

    add redis init

--- src/Services/RedisService/RedisService.ts
+++ src/Services/RedisService/RedisService.ts
@@ -1,7 +1,12 @@
 import IService from '../../Interfaces/IService';
-export default class RedisService implements IService {
-    async  initialize() {
+import Redis from "redis";
 
+export default class RedisService implements IService {
+    public address = "127.0.0.1"
+    public prot = 6379
+    public client = Redis.createClient(this.prot, this.address);
+
+    async  initialize() {
     }
 
 }
