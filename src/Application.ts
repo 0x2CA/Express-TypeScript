fc30a315cc33ca577f2d60a5d606c1f1423b639f
0x2CA <2478557459@qq.com>
2020-03-02 10:56:41 +0800

    add Serice

--- src/Application.ts
+++ src/Application.ts
@@ -1,11 +1,23 @@
 import ExpressService from "./Services/ExpressService/Service"
 
-export default class Application {
+class Application {
+
     public static main(argv: string[]) {
         console.log("运行参数:", ...argv)
-        const express: ExpressService = new ExpressService();
-        express.initialize();
+        this.initialize();
+    }
+
+    private static async  initialize() {
+        await Application.Services.express.initialize();
+    }
+}
+
+namespace Application {
+    export namespace Services {
+        export const express: ExpressService = new ExpressService();
     }
 }
 
 Application.main(process.argv)
+
+export default Application;
--- src/Interfaces/IService.ts
+++ src/Interfaces/IService.ts
@@ -1,3 +1,3 @@
-export default interface Service {
+export default interface IService {
     initialize(): Promise<void>;
 }
