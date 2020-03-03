285b658407e11bded5a05dee8338c9dad5787d05
0x2CA <2478557459@qq.com>
2020-03-03 11:20:13 +0800

    mongdb add user passwd

--- src/Services/MongodbService/MongodbService.ts
+++ src/Services/MongodbService/MongodbService.ts
@@ -6,11 +6,17 @@ export default class MongodbService implements IService {
     public address = "localhost"
     public prot = 27017
     public dbName = "TestDB"
-    public url = `mongodb://${this.address}:${this.prot}/${this.dbName}`
+    public authSource = this.dbName;
+    public user = ""
+    public passwd = ""
+    public url = `mongodb://${this.user.length > 0 ? this.user + ":" + this.passwd + "@" : ""}${this.address}:${this.prot}/${this.dbName}${this.user.length > 0 ? "?authSource=" + this.authSource : ""}`
     public client: Mongodb.MongoClient | null = null;
 
     async  initialize() {
-        this.client = await Mongodb.MongoClient.connect(this.url, { useNewUrlParser: true, useUnifiedTopology: true });
+        this.client = await Mongodb.MongoClient.connect(this.url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
         console.log(`MongoDB数据库${this.url}连接成功!`)
     }
 }
--- src/Services/RedisService/RedisService.ts
+++ src/Services/RedisService/RedisService.ts
@@ -2,14 +2,19 @@ import IService from '../../Interfaces/IService';
 import Redis from "redis";
 
 export default class RedisService implements IService {
-    public address = "localhost"
-    public prot = 6379
+    public config: Redis.ClientOpts = {
+        host: "localhost",
+        port: 6379
+    }
     public client: Redis.RedisClient | null = null;
 
     async  initialize() {
-        this.client = Redis.createClient(this.prot, this.address)
+        this.client = Redis.createClient(this.config);
         this.client.on("error", (...args) => {
             console.error("Redis出错!", ...args);
+        })
+        this.client.on("connect", () => {
+            console.log("Redis连接成功!");
         })
     }
 }
